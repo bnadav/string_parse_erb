131e4457202c339b0523161a8530bce0a465190d
Nadav Blum <nadav.blum@gmail.com>
2011-09-02 01:14:54 +0300

    Rodc

--- lib/str_parse_erb/str_parse_erb.rb
+++ lib/str_parse_erb/str_parse_erb.rb
@@ -1,5 +1,7 @@
 module StrParseErb
 
+   VERSION = "0.1.0"
+
    def string_parse_erb(template, var_hash, safe_level=4)
      data = VarHash.new(var_hash)
      ERB.new(template, safe_level).result(data.get_binding.taint)
