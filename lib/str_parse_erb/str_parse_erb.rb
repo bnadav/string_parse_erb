d70b35f29b9bcc8c56f1f0c9392d7551287f966b
Nadav Blum <nadav.blum@gmail.com>
2011-09-01 14:28:54 +0300

    Untaint string by default

--- lib/str_parse_erb/str_parse_erb.rb
+++ lib/str_parse_erb/str_parse_erb.rb
@@ -13,7 +13,8 @@ module StrParseErb
   end # ClassMethods
 
   module InstanceMethods
-   def string_parse_erb(template, vars_hash, safe_level=3)
+   def string_parse_erb(template, vars_hash, safe_level=3, untaint = true)
+     template.untaint if untaint and template.tainted?
      data = OpenStruct.new(vars_hash)
      ERB.new(template, safe_level).result(data.send(:binding))
    end
--- spec/string_parse_erb_spec.rb
+++ spec/string_parse_erb_spec.rb
@@ -12,8 +12,21 @@ describe "StringParseErb" do
     string_parse_erb(str, vars_hash).should == "Good morning"
   end
 
-  it "blocks unsecure operations" do
+  it "blocks unsecure operations by default ($SAFE level 3)" do
     str = " 1 + 1 = <%= eval(\"1 + 1 \") %>"
     lambda { string_parse_erb(str, {}) }.should raise_error
   end
+
+  it "allows changing SAFE level" do
+    str = " 1 + 1 = <%= eval(\"1 + 1 \") %>"
+    string_parse_erb(str, {}, 0).should == " 1 + 1 = 2"
+  end
+
+  it "Untaints string by default in order to process external strings" do
+    str = "Good <%= part_of_day %>"
+    vars_hash = {:part_of_day => "morning"}
+    str.taint
+    string_parse_erb(str, vars_hash).should == "Good morning"
+  end
+
 end
