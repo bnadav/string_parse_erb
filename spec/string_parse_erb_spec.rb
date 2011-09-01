3b67e90418d267589549e2c1f279f779467bc47f
Nadav Blum <nadav.blum@gmail.com>
2011-09-01 17:28:37 +0300

    Add spec

--- spec/string_parse_erb_spec.rb
+++ spec/string_parse_erb_spec.rb
@@ -6,15 +6,24 @@ describe "StringParseErb" do
     string_parse_erb(str, {}).should == "Hello World!"
   end
 
+  it "deletes from string, erb fragments which have no match in the variables hash" do
+    str = "Who are <%= person %>"
+    string_parse_erb(str, {:man => "you"}).should == "Who are "
+  end
+
   it "replaces erb fragments with values from vars_hash" do
     str = "Good <%= part_of_day %>, the time is <%= time %>."
     vars_hash = {:part_of_day => "morning", :time => "six o'clock"}
     string_parse_erb(str, vars_hash).should == "Good morning, the time is six o'clock."
   end
 
-  it "blocks unsecure operations by default ($SAFE level 3)" do
+  it "blocks insecure operations by default ($SAFE level 3)" do
     str = " 1 + 1 = <%= eval(\"1 + 1 \") %>"
     lambda { string_parse_erb(str, {}) }.should raise_error
+  end
+
+  it "blocks another insecure operations by default ($SAFE level 3)" do
+    lambda { string_parse_erb("<%= abort %>", {}) }.should raise_error
   end
 
   it "allows changing SAFE level" do
@@ -36,10 +45,16 @@ describe "StringParseErb" do
     lambda { string_parse_erb(str, vars_hash, 1, false) }.should raise_error
   end
 
-  it "Tainted string are processed at SAFE level 0" do
+  it "process tainted at SAFE level 0" do
     str = "Good <%= part_of_day %>"
     vars_hash = {:part_of_day => "morning"}
     str.taint
     string_parse_erb(str, vars_hash, 0, false).should == "Good morning"
+  end
+
+  it "does not bind external variables" do
+    str = "Good <%= part_of_day %>"
+    @part_of_day = part_of_day = @@part_of_day = "morning"
+    string_parse_erb(str, {}).should == "Good "
   end
 end
