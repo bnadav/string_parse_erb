e5ace2f33b15ed3c5af8c9bdb791ad5d5fb6b366
Nadav Blum <nadav.blum@gmail.com>
2011-09-01 14:55:04 +0300

    Add lib path to LOADPATH, delete InstanceMethods and ClassMethods modules

--- lib/str_parse_erb/str_parse_erb.rb
+++ lib/str_parse_erb/str_parse_erb.rb
@@ -1,24 +1,9 @@
 module StrParseErb
 
-  def self.included(base)
-    base.extend         ClassMethods
-    base.class_eval do
-
-    end
-    base.send :include, InstanceMethods
-  end # self.included
-
-  module ClassMethods
-
-  end # ClassMethods
-
-  module InstanceMethods
    def string_parse_erb(template, vars_hash, safe_level=3, untaint = true)
      template.untaint if untaint and template.tainted?
      data = OpenStruct.new(vars_hash)
      ERB.new(template, safe_level).result(data.send(:binding))
    end
-  end # InstanceMethods
-
 
 end
